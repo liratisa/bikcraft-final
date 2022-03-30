92b84e79cd4e91d2e5d42368e1fb6c4da91b3a20
Isabela <itellesdelira@gmail.com>
2022-03-30 18:13:46 -0300

    adicionando consulta à API viacep

--- app.js
+++
@@ -1,7 +0,0 @@
-const accordion = document.querySelectorAll(".accordion");
-
-for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-  });
-}
